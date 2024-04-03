0f8c7d168f85be9972fde5e536e88be7153038bd
alexander <aleksandrpereselkov77@gmail.com>
2024-04-03 20:10:41 +0800

    initial

---
+++ Bootstrap.php
@@ -0,0 +1,130 @@
+<?php
+
+/*
+ * This file is part of the alexander777hub project.
+ *
+ * (c) alexander777hub project <http://github.com/alexander777hub/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace alexander777hub\user;
+
+use Yii;
+use yii\authclient\Collection;
+use yii\base\BootstrapInterface;
+use yii\console\Application as ConsoleApplication;
+use yii\i18n\PhpMessageSource;
+
+/**
+ * Bootstrap class registers module and user application component. It also creates some url rules which will be applied
+ * when UrlManager.enablePrettyUrl is enabled.
+ *
+ * @author Dmitry Erofeev <dmeroff@gmail.com>
+ */
+class Bootstrap implements BootstrapInterface
+{
+    /** @var array Model's map */
+    private $_modelMap = [
+        'User'             => 'alexander777hub\user\models\User',
+        'Account'          => 'alexander777hub\user\models\Account',
+        'Profile'          => 'alexander777hub\user\models\Profile',
+        'Token'            => 'alexander777hub\user\models\Token',
+        'RegistrationForm' => 'alexander777hub\user\models\RegistrationForm',
+        'ResendForm'       => 'alexander777hub\user\models\ResendForm',
+        'LoginForm'        => 'alexander777hub\user\models\LoginForm',
+        'SettingsForm'     => 'alexander777hub\user\models\SettingsForm',
+        'RecoveryForm'     => 'alexander777hub\user\models\RecoveryForm',
+        'UserSearch'       => 'alexander777hub\user\models\UserSearch',
+    ];
+
+    /** @inheritdoc */
+    public function bootstrap($app)
+    {
+        /** @var Module $module */
+        /** @var \yii\db\ActiveRecord $modelName */
+        if ($app->hasModule('user') && ($module = $app->getModule('user')) instanceof Module) {
+            $this->_modelMap = array_merge($this->_modelMap, $module->modelMap);
+            foreach ($this->_modelMap as $name => $definition) {
+                $class = "alexander777hub\\user\\models\\" . $name;
+                Yii::$container->set($class, $definition);
+                $modelName = is_array($definition) ? $definition['class'] : $definition;
+                $module->modelMap[$name] = $modelName;
+                if (in_array($name, ['User', 'Profile', 'Token', 'Account'])) {
+                    Yii::$container->set($name . 'Query', function () use ($modelName) {
+                        return $modelName::find();
+                    });
+                }
+            }
+
+            Yii::$container->setSingleton(Finder::className(), [
+                'userQuery'    => Yii::$container->get('UserQuery'),
+                'profileQuery' => Yii::$container->get('ProfileQuery'),
+                'tokenQuery'   => Yii::$container->get('TokenQuery'),
+                'accountQuery' => Yii::$container->get('AccountQuery'),
+            ]);
+
+            if ($app instanceof ConsoleApplication) {
+                $module->controllerNamespace = 'alexander777hub\user\commands';
+            } else {
+                Yii::$container->set('yii\web\User', [
+                    'enableAutoLogin' => true,
+                    'loginUrl' => ['/user/security/login'],
+                    'identityClass' => $module->modelMap['User'],
+                ]);
+
+                $configUrlRule = [
+                    'prefix' => $module->urlPrefix,
+                    'rules'  => $module->urlRules,
+                ];
+
+                if ($module->urlPrefix != 'user') {
+                    $configUrlRule['routePrefix'] = 'user';
+                }
+
+                $configUrlRule['class'] = 'yii\web\GroupUrlRule';
+                $rule = Yii::createObject($configUrlRule);
+
+                $app->urlManager->addRules([$rule], false);
+
+                if (!$app->has('authClientCollection')) {
+                    $app->set('authClientCollection', [
+                        'class' => Collection::className(),
+                    ]);
+                }
+            }
+
+            if (!isset($app->get('i18n')->translations['user*'])) {
+                $app->get('i18n')->translations['user*'] = [
+                    'class' => PhpMessageSource::className(),
+                    'basePath' => __DIR__ . '/messages',
+                    'sourceLanguage' => 'en-US'
+                ];
+            }
+
+            Yii::$container->set('alexander777hub\user\Mailer', $module->mailer);
+
+            $module->debug = $this->ensureCorrectDebugSetting();
+        }
+    }
+
+    /** Ensure the module is not in DEBUG mode on production environments */
+    public function ensureCorrectDebugSetting()
+    {
+        if (!defined('YII_DEBUG')) {
+            return false;
+        }
+        if (!defined('YII_ENV')) {
+            return false;
+        }
+        if (defined('YII_ENV') && YII_ENV !== 'dev') {
+            return false;
+        }
+        if (defined('YII_DEBUG') && YII_DEBUG !== true) {
+            return false;
+        }
+
+        return Yii::$app->getModule('user')->debug;
+    }
+}
---
+++ traits/ModuleTrait.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace alexander777hub\user\traits;
+
+use alexander777hub\user\Module;
+
+/**
+ * Trait ModuleTrait
+ *
+ * @property-read Module $module
+ * @package dektrium\user\traits
+ */
+trait ModuleTrait
+{
+    /**
+     * @return Module
+     */
+    public function getModule()
+    {
+        return \Yii::$app->getModule('user');
+    }
+
+    /**
+     * @return string
+     */
+    public static function getDb()
+    {
+        return \Yii::$app->getModule('user')->getDb();
+    }
+}
